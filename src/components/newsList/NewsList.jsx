e525454f21b9da8607eaa58e82869ec55260ce41
minwoo <qfaq159@naver.com>
2021-07-30 19:45:24 +0900

    NewsList 스피너 추가

--- src/components/newsList/NewsList.jsx
+++ src/components/newsList/NewsList.jsx
@@ -1,34 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import NewsListBlock from './NewsListBlock';
 import NewsItem  from './NewsItem';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 
 const NewsList = ({news}) => {
     const [articles, setArticles] = useState(null);
-    const [loading, setLoading] = useState(false);
 
     useEffect(()=>{
+        
         const fetchData = async () =>{
-            setLoading(true);
-            news.mostPopular().then(articles=>setArticles(articles));
-            setLoading(false);
+
+            news.mostPopular().then(articles=>{
+                setArticles(articles);
+            });
+            
         }
         fetchData();
     },[news])
 
-    if (loading) {
-        return <>대기중</>
-    }
-
-    if(!articles){
-        return null;
-    }
 
     return(
         <NewsListBlock>
-            {articles.map(article=>(
-                <NewsItem key={article.url} article={article}/>
-            ))}
+            {!articles ? <CircularProgress /> :articles.map(
+                    article=>(<NewsItem key={article.url} article={article}/>))}
+                
         </NewsListBlock>
     )
 }
